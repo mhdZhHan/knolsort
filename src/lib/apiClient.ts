5cc2dccd98c66a15c71788b8a69deab44e24b295
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-07-30 18:29:58 +0530

    ⚠️ DELETE:  and generate a new API token and made it safe

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,11 +2,10 @@ import axios from "axios"
 
 // axios instance with base URL
 const apiClient = axios.create({
-	baseURL: "http://192.168.1.75:1337/api",
+	baseURL: process.env.EXPO_PUBLIC_API_BASE_URL,
 	headers: {
 		"Content-Type": "application/json",
-		"x-api-key":
-			"60165c14a40cff8ca76e2dc44b080af2c79cf3707a04133b288023e3d5912e9bade177472ad13b6a3af1e83e522620d52e5863e1193bc2bb6b0decdbdae48b88d96acc0f76cb6e626ff798724737fe9fe6aa3717d5e826bdd170e3cdb52f16d9641d9bc804686136a3986af45e44846618d10df50deac19dfef05cdd0680d104",
+		"x-api-key": process.env.EXPO_PUBLIC_STRAPI_API_TOKEN,
 	},
 })
 
