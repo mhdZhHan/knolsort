3f2d14e096fbd4dcb9285904a5fd14d9e4a7362b
Mohammed Shajahan <dev.mohammedsh@gmail.com>
2024-07-30 22:30:43 +0530

    Fix getSlides

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,5 +1,7 @@
 import axios from "axios"
 
+import type { SliderType } from "../types"
+
 // axios instance with base URL
 const apiClient = axios.create({
 	baseURL: process.env.EXPO_PUBLIC_API_BASE_URL,
@@ -19,12 +21,21 @@ const apiClient = axios.create({
 // }
 
 // API call: Get Sliders
-export const getSliders = async () => {
+export const getSliders = async (): Promise<SliderType[]> => {
 	try {
-		const response = await apiClient.get("/sliders/1?populate=*")
-		return response.data
+		const response = await apiClient.get("/sliders?populate=*")
+
+		const data = response.data.data.map((item: any) => {
+			return {
+				id: item.id,
+				name: item.attributes.name,
+				image: item.attributes.image.data.attributes.url,
+			}
+		})
+
+		return data
 	} catch (error) {
-		console.error("Error fetching user data:", error)
+		console.error("Error fetching slider data:", error)
 		throw error
 	}
 }
